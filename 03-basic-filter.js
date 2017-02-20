37798159d1dcf1c8bf6321419432d4a1cd12bc11
markphd <radical.mark@gmail.com>
2017-02-20 12:14:19 +0400

    Filter passed!

--- 03-basic-filter.js
+++ 03-basic-filter.js
@@ -1,9 +1,20 @@
 function getShortMessages(messages) {
+	var shortMsgs = []
 	messages.filter(function(msg) {
 		if (msg.message.length < 50) {
-			return msg.message;
+			shortMsgs.push(msg.message); 
 		}
 	})
+	return shortMsgs;
 }
 
 module.exports = getShortMessages;
+
+// official solution:
+// module.exports = function getShortMessages(messages) {
+//   return messages.filter(function(item) {
+//     return item.message.length < 50
+//   }).map(function(item) {
+//     return item.message
+//   })
+// }
